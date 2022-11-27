216dd10f321da9cb7a26db549225dd2a0a9daf95
jihen <jihen.najlaoui@esprit.tn>
2022-11-27 14:27:32 +0100

    fist comit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
+import { FrontLayoutComponent } from './layout/front-layout/front-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'',component:FrontLayoutComponent,children:[
+  {path:'',loadChildren:()=>import('./views/front/home/home.module').then(m=>m.HomeModule)},
+  {path:'loginuser',loadChildren:()=>import('./views/front/loginuser/loginuser.module').then(m=>m.LoginuserModule)}
+
+
+] },
+
+
+{path:'admin',component:AdminLayoutComponent,children:[
+  {path:'',loadChildren:()=>import('./views/admin/dashboard/dashboard.module').then(m=>m.DashboardModule)},
+{path:'dashboard',loadChildren:()=>import('./views/admin/dashboard/dashboard.module').then(m=>m.DashboardModule)},
+{path:'loginadmin',loadChildren:()=>import('./views/admin/loginadmin/loginadmin.module').then(m=>m.LoginadminModule)}
+
+]}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
